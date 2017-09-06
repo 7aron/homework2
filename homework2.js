f146d796921373c631e085995ea0509b581fee63
root <root@localhost.localdomain>
2017-09-06 20:25:33 +0000

    first commit

---
+++ homework2.js
@@ -0,0 +1,13 @@
+const num1 = 10;
+const num2 = 20;
+console.log(num1 + num2);
+
+const firstTxt = "It is the first part "
+const secondTxt = "and it is the second one"
+console.log(firstTxt  + secondTxt);
+
+console.log(((25 * 4) - 73) / 9) ;
+console.log("Arm"+"enia");
+console.log((100 / 5) - (20 * 10) + 400);
+
+
