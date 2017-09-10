567cd191763c63b57f5c9aec76508703d1022e0d
7aron <31661360+7aron@users.noreply.github.com>
2017-09-10 11:04:44 +0000

    Add files via upload

--- homework2.js
+++ homework2.js
@@ -10,4 +10,11 @@ console.log(((25 * 4) - 73) / 9) ;
 console.log("Arm"+"enia");
 console.log((100 / 5) - (20 * 10) + 400);
 
-
+const age = 50;
+if (age === 18 ){
+ alert("go to Army!!!");
+} else if (age > 27) {
+ alert("it is already late");
+} else {
+    alert("relax");
+}
